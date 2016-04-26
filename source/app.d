153d8db0b234f5d0f750666735812d4d004ba83e
jython234 <jython234@users.noreply.github.com>
2016-04-25 19:38:35 -0500

    Restart

--- source/app.d
+++ source/app.d
@@ -1,39 +1,5 @@
 import std.stdio;
-import std.conv;
-import std.array;
-import draklib.DRakLib;
-import draklib.util.Logger;
-import draklib.server.RakNetServer;
 
 int main() {
-	Logger l = new LoggerImpl();
-	ServerOptions options = ServerOptions();
-	options.broadcastName = "MCPE;A DRakLib Server;46;0.15.0;0;0";
-	RakNetServer server = new RakNetServer(l, cast(ushort) 19132, "0.0.0.0", options);
-	server.start();
-	int code;
-	server.hasCrashed() ? code = 1 : code = 2;
-	return code;
-}
-
-public class LoggerImpl : Logger {
-	override {
-		public void logDebug(in string message) {
-			writeln("[DEBUG]: " ~ message);
-		}
-		public void logInfo(in string message) {
-			writeln("[INFO]: " ~ message);
-		}
-		public void logWarn(in string message) {
-			writeln("[WARN]: " ~ message);
-		}
-		public void logError(in string message) {
-			writeln("[ERROR]: " ~ message);
-		}
-		public void logTrace(in string fullTrace) {
-			foreach(string segment; split(fullTrace, "\n")) {
-				writeln("[TRACE]: " ~ segment);
-			}
-		}
-	}
+    return 0;
 }
--- source/draklib/util/Logger.d
+++
@@ -1,9 +0,0 @@
-﻿module draklib.util.Logger;
-
-interface Logger {
-	public void logDebug(in string message);
-	public void logInfo(in string message);
-	public void logWarn(in string message);
-	public void logError(in string message);
-	public void logTrace(in string fullTrace);
-}
